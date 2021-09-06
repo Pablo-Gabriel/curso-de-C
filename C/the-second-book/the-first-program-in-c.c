6394f592b8d7f962e14d56e76370b0259ce4cdf3
Pablo Gabriel <pablogabriel.devweb@gmail.com>
2021-09-06 17:04:09 -0300

    Comando de printf

--- C/the-second-book/the-first-program-in-c.c
+++ C/the-second-book/the-first-program-in-c.c
@@ -21,3 +21,11 @@
     return 0;
 } */ 
 
+main() {
+
+    printf("Collum A \t Collum B \t Collum C");
+    printf("\n My computer\'s Beep soubds like this: \a!\n");
+    printf("\" Letz\bs fix that typo and then show the backslash ");
+    printf("character \\\" she said \n");
+    return 0;
+}
