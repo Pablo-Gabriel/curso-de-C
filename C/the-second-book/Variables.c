682a8389467d087fc4e86de8c62c34f6d2e39adc
Pablo Gabriel <pablogabriel.devweb@gmail.com>
2021-09-06 21:00:07 -0300

    The complete code

--- C/the-second-book/Variables.c
+++ C/the-second-book/Variables.c
@@ -22,5 +22,28 @@ main() {
     printf("%c%c needs %d pencils, %d notebooks, and 1 lunchbox\n", firstInitial, middleInitial, number_of_pencils, number_of_notebooks);
     printf("The total cost is $%.2f\n\n", number_of_pencils*pencils + number_of_notebooks*notebooks + lunchbox);
 
+    /* Second child */
+    firstInitial = 'A';
+    middleInitial = 'J';
 
+    number_of_pencils = 10;
+    number_of_notebooks = 3;
+
+    printf("%c%c needs %d pencils, %d notebooks, and 1 lunchbox\n", firstInitial, middleInitial, number_of_pencils, number_of_notebooks);
+    
+    printf("The total cost is $%.2f\n\n", number_of_pencils*pencils + number_of_notebooks*notebooks + lunchbox);
+
+
+    /* Third child */
+
+    firstInitial = 'M';
+    middleInitial = 'T';
+
+    number_of_pencils = 9;
+    number_of_notebooks = 2;
+
+    printf("%c%c needs %d pencils, %d notebooks, and 1 lunchbox\n", firstInitial, middleInitial, number_of_pencils, number_of_notebooks);
+    printf("The total cost is $%.2f\n\n",number_of_pencils*pencils + number_of_notebooks*notebooks + lunchbox);
+
+    return 0;
 }
