521f55ff2dea6142c5f8511085dedd802bd79383
pablo <pablogabriel.devweb@gmail.com>
2022-03-05 16:44:11 -0300

    outras mudanças

--- C/Curso-completo/terceiro-arquivo.c
+++ C/Curso-completo/terceiro-arquivo.c
@@ -1,8 +1,8 @@
-/*calculadora de media*/
+/*1º exercicio: calculadora de media*/
 #include<stdio.h>
 #include<math.h>
 
-int main() {
+/* int main() {
     float nota1, nota2, media;
     
     printf("Insira suas duas notas a seguir: \n");
@@ -11,4 +11,32 @@ int main() {
     printf("Sua media é: %.2f \n", media);
 
     return 0;
-}
+} */
+
+/* int main() {
+    float combustivel, distancia, consumo_medio;
+
+    printf("Qual foi o consumo total de combustivel e a distancia percorrida por ele? (em L e em KM) \n");
+    scanf(" %f %f", &combustivel, &distancia);
+    consumo_medio = distancia / combustivel;
+    printf("O consumo medio de combustivel para a distancia foi de: %.1f KM/L \n", consumo_medio);
+
+    return 0;
+} */
+
+/* Caculadora de hipotenusa */
+
+/*int main() {
+
+    float cat1, cat2, hipotenusa;
+
+    printf("Coloque o valor do cat1: \n");
+    scanf(" %f", &cat1);
+    printf("Coloque o valor do cat2: \n");
+    scanf(" %f", &cat2);
+    hipotenusa = sqrt((cat1 * cat1) + (cat2 * cat2));
+    printf("A hipotenusa vale: %.2f \n", hipotenusa);
+    
+}*/
+
+
