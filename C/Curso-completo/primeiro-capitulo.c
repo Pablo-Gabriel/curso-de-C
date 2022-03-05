00901fa100e0687a92eb41b92b7659283ab3eac7
pablo <pablogabriel.devweb@gmail.com>
2022-03-05 15:02:58 -0300

    Primeiros codigos e configurações de compilador

---
+++ C/Curso-completo/primeiro-capitulo.c
@@ -0,0 +1,26 @@
+/* Introdução */
+/* programa para calculo de IMC */
+#include<stdio.h>
+#include<math.h>
+
+/*const int limite = 30;
+
+int main() {
+    float peso, altura, IMC, potencia;
+
+    printf("Qual seu peso e sua altura?\n");
+    scanf(" %f %f",&peso, &altura);
+    potencia = (altura * altura);
+    IMC = peso / potencia;
+
+    if( IMC <= limite) {
+        printf("Você não está obeso!\n");
+    } else {
+        printf("Você está obeso!\n");
+    };
+    
+    printf("Seu IMC é igual a: %.2f \n", IMC);
+
+    return 0;
+} */
+
--- C/the-first-book/fist-program-in-c.c
+++ C/the-first-book/fist-program-in-c.c
@@ -1,30 +1,30 @@
 #include<stdio.h>
 
-// int main () {
-//     printf("Hello Word!\n");
-//     return 0;
-// }
+/* int main () {
+     printf("Hello Word!\n");
+     return 0;
+}*/
 
 /* print Fahrenheit-Celsius Table 
         for fahr = 0, 20, ..., 300
 */
-// int main () {
+int main () {
 
-    // float fahr, celsius;
-    // int lower, upper, step;
+     float fahr, celsius;
+     int lower, upper, step;
 
-    // lower = 0;     // lower limit of temperature table  
-    // upper = 300;   // upper limit  
-    // step = 20;     // step size
+     lower = 0;     // lower limit of temperature table  
+     upper = 300;   // upper limit  
+     step = 20;     // step size
     
-    // fahr = lower; 
-    // while (fahr <= upper) {
-    //     celsius = (5.0 / 9.0) * (fahr - 32.0);
-    //     printf("%3.0f %6.1f\n", fahr, celsius);
-    //     fahr = fahr + step;
-    // }
+     fahr = lower; 
+     while (fahr <= upper) {
+         celsius = (5.0 / 9.0) * (fahr - 32.0);
+         printf("%3.0f %6.1f\n", fahr, celsius);
+         fahr = fahr + step;
+     }
 
-//}
+}
 
 // Celsius to fahrenheit table
 
